65eedb85df7227ab178bdf56e018cb849a556213
Hazvi Nurafgan <hazvinurafgan@gmail.com>
2022-03-11 15:58:09 +0700

    validate pass

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -23,16 +23,6 @@ const login = (req, res, next) => {
     next();
 }
 
-const editProfile = (req, res, next) => {
-    const { body } = req;
-    const editBody = ['name', 'display_name', 'email', 'password', 'image', 'gender_id', 'date_birth', 'address', 'phone_number'];
-    const bodyProperty = Object.keys(body);
-    const isBodyValid =
-        editBody.filter((property) => !bodyProperty.includes(property))
-            .length == 0 ? true : false;
-    if (!isBodyValid) return responseHelprer.error(res, 400, "Invalid Body");
-    next();
-}
 
 const vehicle = (req, res, next) => {
     const { body } = req;
@@ -48,7 +38,6 @@ const vehicle = (req, res, next) => {
 
 module.exports = {
     register,
-    editProfile,
     vehicle,
     login
 }
